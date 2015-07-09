ef603cbd4d0b4aeaec0ae1fa988a2179571ecd80
Jonas Weissensel <INstinct42@users.noreply.github.com>
2015-07-09 12:19:56 +0200

    Bugfix: Error if calling `git` without parameters.

--- fish/functions/git.fish
+++ fish/functions/git.fish
@@ -1,14 +1,18 @@
 function git
-	set -l git_command $argv[1]
-	switch $git_command
-	case clone
-		if test 1 -le (echo $argv[2] | grep -c https://) -o (echo $argv | wc -w) -gt 2
-			command git $argv
-		else
-			set -l git_url (echo $argv[2] | sed -e 's#\(.*\)/\(.*\)#https://github.com/\1/\2\.git#g')
-			command git clone $git_url
-		end
-	case '*'
-		command git $argv
+	if test -z $argv
+        command git $argv
+    else
+        set -l git_command $argv[1]
+        switch $git_command
+        case clone
+            if test 1 -le (echo $argv[2] | grep -c https://) -o (echo $argv | wc -w) -gt 2
+                command git $argv
+            else
+                set -l git_url (echo $argv[2] | sed -e 's#\(.*\)/\(.*\)#https://github.com/\1/\2\.git#g')
+                command git clone $git_url
+            end
+        case '*'
+            command git $argv
+        end
 	end
 end
